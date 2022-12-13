7e68aab4660a00d20fba41bc872ef7dd02312729
Tristan <75189508+realTristan@users.noreply.github.com>
2022-12-13 08:26:06 -0500

    Update 2. Dictionaries.py

--- 2. Dictionaries.py
+++ 2. Dictionaries.py
@@ -5,15 +5,28 @@ dictionary = {
     "name": "tristan",
     "age": 16
 }
+print(dictionary)
+
 
 # // Accessing Dictionaries
-dictionary_1_name = dictionary["name"]
+dictionary_name = dictionary["name"]
+print(dictionary_name)
+
 
 # // Setting key in dictionary
 dictionary["name"] = "daniel"
+print(dictionary)
+
 
 # // Delete a key from a dictionary
 del dictionary["name"]
+print(dictionary)
+
 
 # // Get length of a dictionary
-dictionary_1_length = len(dictionary)
+dictionary_length = len(dictionary)
+
+
+# // Use the .items() to iterate over a dictionary
+for key, value in dictionary.items():
+  print(f"{key}:{value}")
